5fdcf4ca4378b8f0bad98700c161c90a55d31dfb
Foster Oppong <oppongyaw049@gmail.com>
2025-06-08 23:30:17 -0400

    feat: refresh route

---
+++ controllers/refreshController.js
@@ -0,0 +1,8 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const handleRefresh = async (req, res) => {
+
+};
+
+module.exports = {handleRefresh};
--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3500;
 
+// mongodb connection
 connectDB();
 
+// middleware
+app.use(cookieParser());
 app.use(express.json());
 
+// app routes
 app.use('/register', require('./routes/register'));
+app.use('/login', require('./routes/login'));
 
 mongoose.connection.once('open', () => {
     console.log("MongoDB connected!");
